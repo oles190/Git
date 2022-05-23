ab14ab372b752f72f8bee5953be359278d1814e2
Oles Doskuch <79111158+oles190@users.noreply.github.com>
2022-05-23 19:24:30 +0300

    sdfsa

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,18 @@
 public class Test {
 
+    public static void main(String[] args) {
+
+        System.out.println(
+                "gef"
+
+        );
+
+
+        System.out.println("sd");
+        System.out.println("sd");
+        System.out.println("sd");
+
+    }
 
 }
 
